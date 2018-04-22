d423b19b19b56057d075ddfc0b7bcb28f655b02b
Mac gu <guyuanshan@MacdeMacBook-Pro.local>
2018-04-22 16:23:19 +0800

    UserDefault.stand

--- Todoey/Todoey/TodoListViewController.swift
+++ Todoey/Todoey/TodoListViewController.swift
@@ -11,9 +11,13 @@ import UIKit
 class TodoListViewController: UITableViewController {
     
     var itemArray = ["Find Mike","Buy Eggos","Destory Demogorgon"]
+    let defaults = UserDefaults.standard
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        if let items = defaults.array(forKey: "ToDoListArray") as? [String] {
+            itemArray = items
+        }
         
     }
     //mark-: Tableview Datasource Methods
@@ -47,6 +51,7 @@ class TodoListViewController: UITableViewController {
             }
         let action = UIAlertAction(title: "add new item", style: .default) { (action) in
             self.itemArray.append(textField.text!)
+            self.defaults.set(self.itemArray, forKey: "ToDoListArray")
             self.tableView.reloadData()
         }
       
